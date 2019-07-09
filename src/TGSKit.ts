34e637275c6020aa3e0277b432d2817bbc51d716
Jawish Hameed <jawish@gmail.com>
2019-07-09 17:47:05 +0800

    Added better validation and loading larger files

--- src/TGSKit.ts
+++ src/TGSKit.ts
@@ -1,5 +1,10 @@
 import * as pako from 'pako';
 
+interface IValidationError {
+  message: string;
+  details?: IValidationError[];
+}
+
 export class TGSKit {
   private _data: any = {};
 
@@ -33,46 +38,6 @@ export class TGSKit {
   }
 
   /**
-   * Validates that the Bodymovin JSON meets the requirements for
-   * a Telegram Sticker (tgs).
-   */
-  public validate(): string[] {
-    const errors: string[] = [];
-
-    if (((this._data.op - this._data.ip) / this._data.fr) > 3.0) {
-      errors.push('Longer than 3 seconds');
-    }
-
-    if (this._data.w != 512 || this._data.h != 512) {
-      errors.push('Dimensions should be exactly 512pxx512px');
-    }
-
-    if (this._data.ddd != null && this._data.ddd != 0) {
-      errors.push('Must not have 3D layers');
-    }
-
-    if ('markers' in this._data) {
-      errors.push('Must not have markers');
-    }
-
-    if ('assets' in this._data && Array.isArray(this._data.assets)) {
-      this._data.assets.forEach((asset: any) => {
-        errors.concat(this.checkLayer(asset.layers));
-      });
-    }
-
-    if ('layers' in this._data && Array.isArray(this._data.layers)) {
-      this._data.layers.forEach((layer: any) => {
-        errors.concat(this.checkLayer(layer));
-      });
-    } else {
-      errors.push('Must have atleast 1 layer');
-    }
-
-    return errors;
-  }
-
-  /**
    * Parse a resource into a JSON object
    */
   public parseJSON(src: string): object | false {
@@ -101,8 +66,18 @@ export class TGSKit {
     // Add TGS attribute
     this._data.tgs = 1;
 
-    // GZip and return
-    return pako.gzip(JSON.stringify(this._data));
+    // Remove markers, fonts and chars
+    delete this._data.markers;
+    delete this._data.fonts;
+    delete this._data.chars;
+
+    // GZip
+    const gzByteArray = pako.gzip(JSON.stringify(this._data));
+
+    // Create gzip blob
+    const blob = new Blob([gzByteArray], { type: 'application/gzip' });
+
+    return blob;
   }
 
   /**
@@ -113,8 +88,8 @@ export class TGSKit {
       return;
     }
 
-    const blob = new Blob([this.generate()], { type: 'application/gzip' });
-    const url = URL.createObjectURL(blob)
+    // Generate gzip and create object URL
+    const url = URL.createObjectURL(this.generate());
 
     var element = document.createElement('a');
     element.setAttribute('href', url);
@@ -141,12 +116,15 @@ export class TGSKit {
           try {
             // Attempt to convert it to JSON as is:
 
-            // @ts-ignore
-            const data = String.fromCharCode.apply(null, new Uint8Array(xhr.response as ArrayBuffer));
+            // Convert arraybuffer to string
+            const data = new Uint8Array(xhr.response).reduce((data, byte) => {
+              return data + String.fromCharCode(byte);
+            }, '');
+
+            // Attempt to parse the data into JSON
             return resolve(JSON.parse(data));
           } catch (err) {
             // Attempt to ungzip response and convert to JSON:
-
             try {
               const data = pako.inflate(xhr.response, { to: 'string' });
               return resolve(JSON.parse(data));
@@ -159,77 +137,166 @@ export class TGSKit {
     });
   }
 
-  private checkLayer(layer: any): string[] {
-    const errors: string[] = [];
+  /**
+   * Validates that the Bodymovin JSON meets the requirements for
+   * a Telegram Sticker (tgs).
+   */
+  public validate(): IValidationError[] {
+    const errors: IValidationError[] = [];
 
-    if (layer.ddd != null && layer.ddd != 0) {
-      errors.push('Composition should not include any 3D Layers');
+    if (this._data.fr != 30 && this._data.fr != 60) {
+      errors.push({ message: 'Frame rate must be exactly 30 or 60' });
     }
 
-    if (layer.sr != null && layer.sr != 1) {
-      errors.push('Composition should not include any Time Stretching');
+    if (((this._data.op - this._data.ip) / this._data.fr) > 3.0) {
+      errors.push({ message: 'Should not be longer than 3 seconds' });
     }
 
-    if (layer.tm != null) {
-      errors.push('Composition should not include any Time Remapping');
+    if (this._data.w != 512 || this._data.h != 512) {
+      errors.push({ message: 'Dimensions should be exactly 512pxx512px' });
     }
 
-    if (layer.ty === 1) {
-      errors.push('Composition should not include any Solids');
+    if (this._data.ddd != null && this._data.ddd != 0) {
+      errors.push({ message: 'Should not have 3D layers' });
     }
 
-    if (layer.ty === 2) {
-      errors.push('Composition should not include any Images');
+    if ('markers' in this._data) {
+      // TODO: Is this check necessary?
+      // errors.push('Must not have markers');
     }
 
-    if (layer.ty === 5) {
-      errors.push('Composition should not include any Texts');
+    if ('assets' in this._data && Array.isArray(this._data.assets)) {
+      const assetsErrors: IValidationError[] = [];
+
+      this._data.assets.forEach((asset: any, i: number) => {
+        if ('layers' in asset && Array.isArray(asset.layers)) {
+          const assetErrors: IValidationError[] = this.checkLayer(asset.layers);
+
+          if (assetErrors.length !== 0) {
+            assetsErrors.push({ message: `Asset ${asset.id || i} has errors`, details: assetErrors });
+          }
+        }
+      });
+
+      if (assetsErrors.length !== 0) {
+        errors.push({ message: 'Assets must not have errors', details: assetsErrors })
+      }
     }
 
-    if (layer.hasMask === true || layer.masksProperties != null) {
-      errors.push('Composition should not include any Masks');
+    if ('layers' in this._data && Array.isArray(this._data.layers)) {
+      const layersErrors: IValidationError[] = [];
+
+      this._data.layers.forEach((layer: any) => {
+        const layerErrors: IValidationError[] = this.checkLayer(layer);
+
+        if (layerErrors.length !== 0) {
+          layersErrors.push({ message: `Layer `, details: layerErrors });
+        }
+
+        if (layersErrors.length !== 0) {
+          errors.push({ message: 'Layers have errors', details: layersErrors })
+        }
+      });
+    } else {
+      errors.push({ message: 'Should have atleast 1 layer' });
     }
 
-    if (layer.tt != null) {
-      errors.push('Composition should not include any Mattes');
+    if ('fonts' in this._data && Array.isArray(this._data.fonts) && this._data.fonts.length > 0) {
+      errors.push({ message: 'Should not have fonts' });
     }
 
-    if (layer.ao === 1) {
-      errors.push('Composition should not include any Auto-Oriented Layers');
+    if ('chars' in this._data && Array.isArray(this._data.chars) && this._data.chars.length > 0) {
+      errors.push({ message: 'Should not have glyph chars' });
     }
 
-    if (layer.ef != null) {
-      errors.push('Composition should not include any Layer Effects');
+    if (this.generate().size > 65536) {
+      errors.push({ message: 'Total sticker size should not exceed 64 KB' });
     }
-
-    errors.concat(this.checkItems(layer.shapes, true));
 
     return errors;
   }
 
-  private checkItems(items: any, shapes: any): string[] {
-    const errors: string[] = [];
+  private checkLayer(layer: any): IValidationError[] {
+    const errors: IValidationError[] = [];
+
+    if (layer.ddd != null && layer.ddd != 0) {
+      errors.push({ message: 'Composition should not include any 3D Layers' });
+    }
+
+    if (layer.sr != null && layer.sr != 1) {
+      errors.push({ message: 'Composition should not include any Time Stretching' });
+    }
+
+    if (layer.tm != null) {
+      errors.push({ message: 'Composition should not include any Time Remapping' });
+    }
+
+    if (layer.ty === 1) {
+      errors.push({ message: 'Composition should not include any Solids' });
+    }
+
+    if (layer.ty === 2) {
+      errors.push({ message: 'Composition should not include any Images' });
+    }
+
+    if (layer.ty === 5) {
+      errors.push({ message: 'Composition should not include any Texts' });
+    }
+
+    if (layer.hasMask === true || layer.masksProperties != null) {
+      errors.push({ message: 'Composition should not include any Masks' });
+    }
+
+    if (layer.tt != null) {
+      errors.push({ message: 'Composition should not include any Mattes' });
+    }
+
+    if (layer.ao === 1) {
+      errors.push({ message: 'Composition should not include any Auto-Oriented Layers' });
+    }
+
+    if (layer.ef != null) {
+      errors.push({ message: 'Composition should not include any Layer Effects' });
+    }
+
+    if ('shapes' in layer && Array.isArray(layer.shapes)) {
+      const shapesErrors: IValidationError[] = this.checkItems(layer.shapes, true);
+
+      if (shapesErrors.length !== 0) {
+        errors.push({ message: 'Composition shapes should not have errors', details: shapesErrors });
+      }
+    }
+
+    return errors;
+  }
+
+  private checkItems(items: any, shapes: any): IValidationError[] {
+    const errors: IValidationError[] = [];
 
     if (items != null) {
       items.forEach((item: any) => {
         if (item.ty == 'rp') {
-          errors.push('Composition should not include any Repeaters');
+          errors.push({ message: 'Composition should not include any Repeaters' });
         }
 
         if (item.ty == 'sr') {
-          errors.push('Composition should not include any Star Shapes');
+          errors.push({ message: 'Composition should not include any Star Shapes' });
         }
 
         if (item.ty == 'mm') {
-          errors.push('Composition should not include any Merge Paths');
+          errors.push({ message: 'Composition should not include any Merge Paths' });
         }
 
         if (item.ty == 'gs') {
-          errors.push('Composition should not include any Gradient Strokes');
+          errors.push({ message: 'Composition should not include any Gradient Strokes' });
         }
 
         if (shapes === true) {
-          errors.concat(this.checkItems(item.it, false));
+          const itemErrors: IValidationError[] = this.checkItems(item.it, false);
+
+          if (itemErrors.length !== 0) {
+            errors.push({ message: 'Composition items should not have errors', details: itemErrors });
+          }
         }
       });
     }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 import { TGSKit } from './TGSKit';
 
-export { TGSKit };
-
 // @ts-ignore
 window.TGSKit = TGSKit;
